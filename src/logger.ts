ecc7e0ecdf22b1f90c277159faf77c1647b2145b
Dmitry Andriyanov <dima117a@yandex-team.ru>
2023-05-12 17:13:39 +0300

    change logger interface

--- src/logger.ts
+++ src/logger.ts
@@ -1,74 +1,26 @@
 import { createContext, useContext } from "react";
 
 export enum LogLevel {
-  /** Log level for very low severity diagnostic messages. */
-  Trace = 0,
-  /** Log level for low severity diagnostic messages. */
-  Debug = 1,
-  /** Log level for informational diagnostic messages. */
-  Information = 2,
-  /** Log level for diagnostic messages that indicate a non-fatal problem. */
-  Warning = 3,
-  /** Log level for diagnostic messages that indicate a failure in the current operation. */
-  Error = 4,
-  /** Log level for diagnostic messages that indicate a failure that will terminate the entire application. */
-  Fatal = 5,
+  Trace = 10,
+  Debug = 20,
+  Information = 30,
+  Warning = 40,
+  Error = 50,
+  Fatal = 60,
 }
 
-export type LoggerArgument = boolean | number | string | null;
-
 export type JsonValue =
-  | JsonObject
-  | JsonValue[]
-  | boolean
-  | number
   | string
-  | readonly JsonValue[]
+  | number
+  | boolean
+  | { [x: string]: JsonValue }
+  | Array<JsonValue>
   | null
   | undefined;
 
-export type JsonObject = {
-  [k: string]: JsonValue;
-};
-
-export type LogMethod = {
-  <T extends string = string>(
-    template: string,
-    a?: T extends `${string}%${string}` ? LoggerArgument : never,
-    b?: LoggerArgument,
-    c?: LoggerArgument,
-    d?: LoggerArgument,
-    e?: LoggerArgument,
-    f?: LoggerArgument
-  ): void;
-};
-
-export abstract class Logger {
-  abstract log<T extends string = string>(
-    level: LogLevel,
-    template: string,
-    a?: T extends `${string}%${string}` ? LoggerArgument : never,
-    b?: LoggerArgument,
-    c?: LoggerArgument,
-    d?: LoggerArgument,
-    e?: LoggerArgument,
-    f?: LoggerArgument
-  ): void;
-
-  abstract child<T extends JsonObject>(context: T): Logger;
-
-  trace: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Trace, t, a, b, c, d, e, f);
-  debug: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Debug, t, a, b, c, d, e, f);
-  info: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Information, t, a, b, c, d, e, f);
-  warn: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Warning, t, a, b, c, d, e, f);
-  error: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Error, t, a, b, c, d, e, f);
-  fatal: LogMethod = (t, a, b, c, d, e, f) =>
-    this.log(LogLevel.Fatal, t, a, b, c, d, e, f);
+export interface Logger {
+  log(level: LogLevel, message: string): void;
+  child(context: Record<string, JsonValue>): Logger;
 }
 
 const context = createContext<Logger | undefined>(undefined);
