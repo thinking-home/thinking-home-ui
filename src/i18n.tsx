dc53aa68d26ba55f741a43377c88838de5fabbe5
Dmitry Andriyanov <dima117a@yandex-team.ru>
2024-05-19 11:04:11 +0300

    i18n

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -1,22 +1,31 @@
 import { createContext, useContext, useMemo } from "react";
-import { Ctx, I18n, Keyset, Params } from "@thinking-home/i18n";
+import { Ctx, Keyset, Content, Params } from "@thinking-home/i18n";
 
 export const context = createContext<Ctx | undefined>(undefined);
 
 export const Provider = context.Provider;
 
-export const useKeyset = <A extends Keyset>(keyset: A) => {
+export const useI18nContext = () => {
   const ctx = useContext(context);
-  const i18n = useMemo(() => new I18n(keyset, ctx!), [keyset, ctx]);
 
   if (!ctx) {
-    throw new Error("");
+    throw new Error("i18n context is undefined");
   }
 
-  const t = <K extends keyof A>(key: K, params: Params<A[K]["type"]>) =>
-    i18n.translate(key, params);
-  const raw = <K extends keyof A>(key: K, params: Params<A[K]["type"]>) =>
-    i18n.translateRaw(key, params);
+  return ctx;
+};
 
-  return { t, raw };
+export const useKeyset = <A extends Record<string, Content>>(
+  keyset: Keyset<A>
+) => {
+  const ctx = useI18nContext();
+
+  return useMemo(() => {
+    const t = <K extends keyof A>(key: K, params: Params<A[K]["type"]>) =>
+      keyset.translate(ctx, key, params);
+    const raw = <K extends keyof A>(key: K, params: Params<A[K]["type"]>) =>
+      keyset.translateRaw(ctx, key, params);
+
+    return { t, raw };
+  }, [keyset, ctx]);
 };
